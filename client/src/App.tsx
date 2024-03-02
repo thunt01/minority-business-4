56aab2adf2b9e59f3e2ea0df20348b4f97ca3413
anniamatthews <amatts2002@gmail.com>
2024-03-01 22:29:33 -0500

    landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,25 +2,48 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home'
 import About from './components/About'
+import Work from './components/Work'
+import Footer from './components/Footer'
 import Contact from './components/Contact'
 import Signup from "./components/Signup"
 import Nav from './components/Nav'
 import Checkout from './components/Checkout';
 
 
-const App = () => {
-    return (
-        <div>
-            <Nav />
-            <Routes>
-                <Route path="/" element={ <Home /> }/>
-                <Route path="/about" element={ <About /> }/>
-                <Route path="/contact" element={ <Contact /> }/>
-                <Route path="/signup" element={ <Signup /> }/>
-                <Route path="/checkout" element={<Checkout/>}/>
-            </Routes>
+
+function App(){
+    return (    
+        <div className='App'>
+            <Home/>
+            <About/>
+            <Work/>
+            <Footer/>
         </div>
-    )    
+
+
+
+
+    )
 }
+
+
+
+
+
+// const App = () => {
+//     return (
+//         <div>
+
+//             <Nav />
+//             <Routes>
+//                 <Route path="/" element={ <Home /> }/>
+//                 <Route path="/about" element={ <About /> }/>
+//                 <Route path="/contact" element={ <Contact /> }/>
+//                 <Route path="/signup" element={ <Signup /> }/>
+//                 <Route path="/checkout" element={<Checkout/>}/>
+//             </Routes>
+//         </div>
+//     )    
+// }
 
 export default App;
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import '../App.css'
+import Logo from "../assets/Logo.svg"
+import CultureCartLogo from "../assets/CultureCartLogo.png"
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import Diversity3Icon from '@mui/icons-material/Diversity3';
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+
+const Navbar = () => {
+
+    const [openMenu, setOpenMenu] = useState(false)
+    const menuOptions = [
+        {
+            text: "Home",
+            icon: <HomeIcon/>
+        }, 
+        {
+            text: "About", 
+            icon: <InfoIcon/>
+        },
+        {
+            text: "Team",
+            icon: <Diversity3Icon/>
+        }
+
+    ]
+
+
+
+    return(
+        <nav>
+            <div className="nav-logo-container">
+                <img style= {{width: 150, height: 150}}src={CultureCartLogo} alt=""/>
+            </div>
+            <div className="navbar-links-container">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Team</a>
+
+                
+                <button className="primary-button">Log In</button>             
+            </div>
+            <Drawer open = {openMenu} onClose={() => setOpenMenu(false)} 
+            anchor = "right">
+                <Box
+                    sx={{ width: 250 }}
+                    role="presentation"
+                    onClick={() => setOpenMenu(false)}
+                    onKeyDown={() => setOpenMenu(false)}
+                >
+          <List>
+            {menuOptions.map((item) => (
+                <ListItem key={item.text} disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>{item.icon}</ListItemIcon>
+                        <ListItemText primary={item.text} />
+                    </ListItemButton>
+                </ListItem>
+            ))}
+                </List>
+                <Divider />
+                </Box>
+            </Drawer>
+        </nav>
+    )
+
+
+};
+
+export default Navbar;
