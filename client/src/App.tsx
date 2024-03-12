98e9fd99a497e2b5891197e916cedde5a0aca8dc
anniamatthews <amatts2002@gmail.com>
2024-03-12 13:37:51 -0400

    home page updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,9 +19,7 @@ function App(){
             <Work/>
             <Footer/>
         </div>
-
-
-
+        
 
     )
 }
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -18,66 +18,82 @@ import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
 import Diversity3Icon from '@mui/icons-material/Diversity3';
 import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
 import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import { Link } from 'react-router-dom';
+
+
 
 const Navbar = () => {
-
-    const [openMenu, setOpenMenu] = useState(false)
+    const [openMenu, setOpenMenu] = useState(false);
     const menuOptions = [
-        {
-            text: "Home",
-            icon: <HomeIcon/>
-        }, 
-        {
-            text: "About", 
-            icon: <InfoIcon/>
-        },
-        {
-            text: "Team",
-            icon: <Diversity3Icon/>
-        }
+      {
+        text: "Home",
+        icon: <HomeIcon />,
+      },
+      {
+        text: "About",
+        icon: <InfoIcon />,
+      },
+      {
+        text: "Testimonials",
+        icon: <CommentRoundedIcon />,
+      },
+      {
+        text: "Contact",
+        icon: <PhoneRoundedIcon />,
+      },
+      {
+        text: "Cart",
+        icon: <ShoppingCartRoundedIcon />,
+      },
+    ];
+    return (
+      <nav>
+        <div className="nav-logo-container">
+            <img style= {{width: 75, height: 75}}src={CultureCartLogo} alt=""/>
+            
+        </div>
+        <div className="navbar-links-container">
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Team</a>
 
-    ]
+            <Link to="/Login" className="primary-button">Log in</Link>
 
 
 
-    return(
-        <nav>
-            <div className="nav-logo-container">
-                <img style= {{width: 150, height: 150}}src={CultureCartLogo} alt=""/>
-            </div>
-            <div className="navbar-links-container">
-                <a href="">Home</a>
-                <a href="">About</a>
-                <a href="">Team</a>
+          {/* <button className="primary-button">Log in</button> */}
 
-                
-                <button className="primary-button">Log In</button>             
-            </div>
-            <Drawer open = {openMenu} onClose={() => setOpenMenu(false)} 
-            anchor = "right">
-                <Box
-                    sx={{ width: 250 }}
-                    role="presentation"
-                    onClick={() => setOpenMenu(false)}
-                    onKeyDown={() => setOpenMenu(false)}
-                >
-          <List>
-            {menuOptions.map((item) => (
+
+
+        </div>
+        <div className="navbar-menu-container">
+          <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+        </div>
+        <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+          <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={() => setOpenMenu(false)}
+            onKeyDown={() => setOpenMenu(false)}
+          >
+            <List>
+              {menuOptions.map((item) => (
                 <ListItem key={item.text} disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>{item.icon}</ListItemIcon>
-                        <ListItemText primary={item.text} />
-                    </ListItemButton>
+                  <ListItemButton>
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.text} />
+                  </ListItemButton>
                 </ListItem>
-            ))}
-                </List>
-                <Divider />
-                </Box>
-            </Drawer>
-        </nav>
-    )
+              ))}
+            </List>
+            <Divider />
+          </Box>
+        </Drawer>
+      </nav>
+    );
+  };
+  
+ 
 
-
-};
 
 export default Navbar;
