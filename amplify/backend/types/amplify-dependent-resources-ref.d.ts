aa0b99a4b443276431ac9504c043dd15d33937a9
zandi h <91852163+zandihudson@users.noreply.github.com>
2024-03-15 13:32:39 -0400

    new error
    Configuration is now working , current error is : Module not found: Error: Can't resolve '@aws-amplify/core/internals/utils' in

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,14 +1,21 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "minoritybusiness43a185504": {
+    "minoritybusiness493b154cd93b154cd": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
       "CreatedSNSRole": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "minoritybusiness493b154cd93b154cdPostConfirmation": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,13 +12,7 @@ import '@aws-amplify/ui-react/styles.css';
 
 
 Amplify.configure(awsExports);
-
-interface User {
-    username: string;
-    // Add other user properties as needed
-  }
-
-  const App: React.FC = () => {
+const App: React.FC = () => {
     return (
 //             <div>
 //                 <nav>
@@ -49,14 +43,14 @@ interface User {
 // // It can be used to handle authentication routes if needed
 // const AuthenticatorWrapper: React.FC = () => {
 //     return (
-    <Authenticator>
-      {({ signOut, user }: { signOut: () => void, user: User | null }) => (
-        <main>
-          <h1>Hello </h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+  <Authenticator>
+  {({ signOut, user }) => (
+    <main>
+      <h1>Hello {user?.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </main>
+  )}
+</Authenticator>
     );
 };
 export default App;
