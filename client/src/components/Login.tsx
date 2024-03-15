530fe072bf721984f5731b95af44c6aa08f47af5
zandi h <91852163+zandihudson@users.noreply.github.com>
2024-03-15 18:08:14 -0400

    Update
    Commenting out login functionality

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,22 +1,26 @@
-import { Amplify } from 'aws-amplify';
-import awsExports from '../aws-exports';
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+// import { Amplify } from 'aws-amplify';
+// import awsExports from '../aws-exports';
+// import { Authenticator } from '@aws-amplify/ui-react';
+// import '@aws-amplify/ui-react/styles.css';
 
-Amplify.configure(awsExports);
+// Amplify.configure(awsExports);
 
 
 
 const Login: React.FC = () => {
     return(
-        <Authenticator>
-        {({ signOut, user }) => (
-          <main>
-            <h1>Hello {user?.username}</h1>
-            <button onClick={signOut}>Sign out</button>
-          </main>
-        )}
-      </Authenticator>
+      //   <Authenticator>
+      //   {({ signOut, user }) => (
+      //     <main>
+      //       <h1>Hello {user?.username}</h1>
+      //       <button onClick={signOut}>Sign out</button>
+      //     </main>
+      //   )}
+      // </Authenticator>
+      <section>
+        <h1> Login Page</h1>
+        
+        </section>
     )    
 }
 
