2776ba17ad736c160074e12f7c074a159ca2f3de
zandi h <91852163+zandihudson@users.noreply.github.com>
2024-03-18 12:29:27 -0400

    Update Login.tsx

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,5 +1,6 @@
 
 import { Amplify } from 'aws-amplify';
+
 import awsmobile from '../aws-exports';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
@@ -7,14 +8,19 @@ import '@aws-amplify/ui-react/styles.css';
 Amplify.configure(awsmobile);
 
 export default function Login() {
+  
   return (
+    
     <Authenticator>
       {({ signOut, user }) => (
+       
         <main>
           <h1>Hello {user?.username}</h1>
           <button onClick={signOut}>Sign out</button>
         </main>
       )}
     </Authenticator>
+    
   );
+  
 }
