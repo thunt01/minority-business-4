f211e817ff1d7164e2c5cc679d1de577f0609059
Mae Kyaw San <maeks@Maes-Laptop.local>
2024-03-22 16:59:52 -0400

    Search results page

--- client/src/components/SearchResults.tsx
+++ client/src/components/SearchResults.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
 import {SearchFn} from './Search';
+import Search from './Search';
+import Navbar from './Navbar';
 
 const SearchResults = () => {
     const searchTag = String(useParams().search_tag);
     return (
+        <div>
+            <Navbar></Navbar>
+            <h1>Search Results</h1>
+            <form>
+                Price range:
+                <input type="text" id="low-price" placeholder="Min"></input>
+                <input type="text" id="high-price" placeholder="Max"></input>
+            </form>
         <SearchFn product_name={searchTag} />
+        </div>
     )
 }
 
