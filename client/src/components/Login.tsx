bffd32e5e3ce672b668c89c39f193070ea16fe00
Mae Kyaw San <maeks@Maes-Laptop.local>
2024-03-26 12:15:07 -0400

    Amplify folders

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,30 +1,21 @@
-
-// import { Amplify } from 'aws-amplify';
-
-// import awsmobile from '../aws-exports';
-// import { Authenticator } from '@aws-amplify/ui-react';
-// import '@aws-amplify/ui-react/styles.css';
-
-// Amplify.configure(awsmobile);
+import awsmobile from '../aws-exports';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import React from "react";
 
+Amplify.configure(awsmobile);
 
 export default function Login() {
-  
   return (
-
-    <div></div>
-    
-  //   <Authenticator>
-  //     {({ signOut, user }) => (
+    <Authenticator>
+      {({ signOut, user }) => (
        
-  //       <main>
-  //         <h1>Hello {user?.username}</h1>
-  //         <button onClick={signOut}>Sign out</button>
-  //       </main>
-  //     )}
-  //   </Authenticator>
-    
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
-  
 }
