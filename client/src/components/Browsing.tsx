5c69d8f3acf8c110007c0a388000d774227aa692
Mae Kyaw San <maeks@Maes-Laptop.local>
2024-04-02 11:54:33 -0400

    Show different browing page if signed in/out

--- client/src/components/Browsing.tsx
+++ client/src/components/Browsing.tsx
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import Search from './Search';
-import Business from './Business';
+import { fetchUserAttributes } from 'aws-amplify/auth';
 
 const Product = () => {
     const [search_results, setResults] = useState([]);
+    const [currentUser, setCurrentUser] = useState('');
+    async function currentAuthenticatedUser() {
+        try {
+            const user_details = await fetchUserAttributes();
+            setCurrentUser(user_details.email);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     useEffect(() => {
         fetch('/products')
             .then((res) => res.json())
             .then((data) => {setResults(data.result)});
     }, []);
     const results = JSON.parse(JSON.stringify(search_results));
-    
-    
-    
+    currentAuthenticatedUser();
     const listItems = results.map((product: any) => (
         <li key={product.Id} className="search-result-item">
             <a href={`/product/${product.ProductID}`}>
@@ -26,18 +34,19 @@ const Product = () => {
         </li>
     ));
 
-
-
     return (
         <div>
             <Navbar></Navbar>
+            {currentUser && (
+                <div>
+                    hi {currentUser}
+                    <h1>Featured Products</h1>
+                </div>
+            )}
+
             <Search></Search>
             
             <h1>Featured</h1>
-
-            <h1>Featured Products</h1>
-            
-
             <h1>Browsing</h1>
             {listItems}
         </div>
