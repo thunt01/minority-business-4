09ceeb42e54299779b6d18014cb2e0de60b93e09
Mae Kyaw San <maeks@Maes-Laptop.local>
2024-04-10 18:59:51 -0400

    CSS changes

--- client/src/components/BusinessForm.tsx
+++ client/src/components/BusinessForm.tsx
@@ -241,7 +241,7 @@ function BusinessForm() {
                           <AttachFileIcon></AttachFileIcon>
                         </i>
                   </div>
-                  <a href="#" type="submit"className="secondary-button">submit</a>
+                  <a href="#" type="submit"className="secondary-button">Submit</a>
                   <a href="#" type="submit"className="secondary-button" onClick={navToSelectProducts}>Click Here to feature products</a>
                   <a href="#" type="submit"className="secondary-button" onClick={navToConfigBusinessPromo}>Click Here to feature business</a>
                   
